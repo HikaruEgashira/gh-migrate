e9a414245b4b4707d2410887a9624a786bb77587
hikae <account@egahika.dev>
2024-09-12 21:42:10 +0900

    Add support for multiple repositories

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"time"
 
 	gh "github.com/cli/go-gh/v2"
@@ -20,95 +21,109 @@ var rootCmd = &cobra.Command{
 	Short: "PRを作成します",
 	Long:  `PRを作成します`,
 	Run: func(cmd *cobra.Command, args []string) {
-		repo := cmd.Flag("repo").Value.String()
-		workPath := os.Getenv("HOME") + "/workspaces/" + repo
-		currentPath, _ := os.Getwd()
+		repos := strings.Split(cmd.Flag("repo").Value.String(), ",")
+		var wg sync.WaitGroup
 
-		titleTemplate := "[gh-migrate]"
-		bodyTemplate := `This PR is created by [gh-migrate](https://github.com/HikaruEgashira/gh-migrate)
+		for _, repo := range repos {
+			wg.Add(1)
+			go func(repo string) {
+				defer wg.Done()
+				processRepo(repo, cmd)
+			}(repo)
+		}
+
+		wg.Wait()
+	},
+}
+
+func processRepo(repo string, cmd *cobra.Command) {
+	workPath := os.Getenv("HOME") + "/workspaces/" + repo
+	currentPath, _ := os.Getwd()
+
+	titleTemplate := "[gh-migrate]"
+	bodyTemplate := `This PR is created by [gh-migrate](https://github.com/HikaruEgashira/gh-migrate)
 ---`
-		branchNameTemplate := "gh-migrate-" + time.Now().Format("20060102150405")
+	branchNameTemplate := "gh-migrate-" + time.Now().Format("20060102150405")
 
-		force := cmd.Flag("force").Value.String()
+	force := cmd.Flag("force").Value.String()
 
-		if force == "true" {
-			err := os.RemoveAll(workPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		_, err := os.Stat(workPath)
+	if force == "true" {
+		err := os.RemoveAll(workPath)
 		if err != nil {
-			cloneArgs := []string{"repo", "clone", repo, workPath, "--", "--depth=1"}
-			_, _, err = gh.Exec(cloneArgs...)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("Repository cloned: %s\n", repo)
-		}
-		os.Chdir(workPath)
-
-		// get default branch
-		stdout, _, _ := gh.Exec("repo", "view", "--json", "defaultBranchRef", "-q", ".defaultBranchRef.name")
-		defaultBranch := strings.TrimSpace(stdout.String())
-
-		// exec command
-		cmdOption := cmd.Flag("cmd").Value.String()
-		if cmdOption != "" {
-			execCommand(cmdOption, &titleTemplate, &bodyTemplate)
-		}
-		shOption := cmd.Flag("sh").Value.String()
-		if shOption != "" {
-			execShellScript(shOption, &titleTemplate, &bodyTemplate, currentPath)
-		}
-		astgrepOption := cmd.Flag("astgrep").Value.String()
-		if astgrepOption != "" {
-			execAstGrep(astgrepOption, &titleTemplate, &bodyTemplate, currentPath)
-		}
-		semgrepOption := cmd.Flag("semgrep").Value.String()
-		if semgrepOption != "" {
-			execSemgrep(semgrepOption, &titleTemplate, &bodyTemplate, currentPath)
-		}
-
-		// create branch
-		exec.Command("git", "switch", "-c", branchNameTemplate).Run()
-		exec.Command("git", "add", ".").Run()
-		statusOutput, _ := exec.Command("git", "status", "--porcelain").Output()
-		fmt.Println(string(statusOutput))
-		if len(statusOutput) == 0 {
-			fmt.Println("No changes to commit. Exiting.")
-			return
-		}
-
-		exec.Command("git", "commit", "-m", titleTemplate).Run()
-		exec.Command("git", "push", "origin", branchNameTemplate).Run()
-
-		// create PR
-		prArgs := []string{
-			"pr",
-			"create",
-			"--base", defaultBranch,
-			"--head", branchNameTemplate,
-			"--title", titleTemplate,
-			"--body", bodyTemplate,
-			"--repo", repo,
-		}
-		stdout, stderr, err := gh.Exec(prArgs...)
-		if err != nil {
-			fmt.Println(stderr.String())
 			log.Fatal(err)
 		}
-		fmt.Println(stdout.String())
+	}
 
-		// open PR
-		if cmd.Flag("open").Value.String() != "" {
-			exec.Command("open", stdout.String()).Run()
+	_, err := os.Stat(workPath)
+	if err != nil {
+		cloneArgs := []string{"repo", "clone", repo, workPath, "--", "--depth=1"}
+		_, _, err = gh.Exec(cloneArgs...)
+		if err != nil {
+			log.Fatal(err)
 		}
-		if cmd.Flag("with-dev").Value.String() != "" {
-			exec.Command("open", strings.ReplaceAll(stdout.String(), "com/", "dev/")).Run()
-		}
-	},
+		fmt.Printf("Repository cloned: %s\n", repo)
+	}
+	os.Chdir(workPath)
+
+	// get default branch
+	stdout, _, _ := gh.Exec("repo", "view", "--json", "defaultBranchRef", "-q", ".defaultBranchRef.name")
+	defaultBranch := strings.TrimSpace(stdout.String())
+
+	// exec command
+	cmdOption := cmd.Flag("cmd").Value.String()
+	if cmdOption != "" {
+		execCommand(cmdOption, &titleTemplate, &bodyTemplate)
+	}
+	shOption := cmd.Flag("sh").Value.String()
+	if shOption != "" {
+		execShellScript(shOption, &titleTemplate, &bodyTemplate, currentPath)
+	}
+	astgrepOption := cmd.Flag("astgrep").Value.String()
+	if astgrepOption != "" {
+		execAstGrep(astgrepOption, &titleTemplate, &bodyTemplate, currentPath)
+	}
+	semgrepOption := cmd.Flag("semgrep").Value.String()
+	if semgrepOption != "" {
+		execSemgrep(semgrepOption, &titleTemplate, &bodyTemplate, currentPath)
+	}
+
+	// create branch
+	exec.Command("git", "switch", "-c", branchNameTemplate).Run()
+	exec.Command("git", "add", ".").Run()
+	statusOutput, _ := exec.Command("git", "status", "--porcelain").Output()
+	fmt.Println(string(statusOutput))
+	if len(statusOutput) == 0 {
+		fmt.Println("No changes to commit. Exiting.")
+		return
+	}
+
+	exec.Command("git", "commit", "-m", titleTemplate).Run()
+	exec.Command("git", "push", "origin", branchNameTemplate).Run()
+
+	// create PR
+	prArgs := []string{
+		"pr",
+		"create",
+		"--base", defaultBranch,
+		"--head", branchNameTemplate,
+		"--title", titleTemplate,
+		"--body", bodyTemplate,
+		"--repo", repo,
+	}
+	stdout, stderr, err := gh.Exec(prArgs...)
+	if err != nil {
+		fmt.Println(stderr.String())
+		log.Fatal(err)
+	}
+	fmt.Println(stdout.String())
+
+	// open PR
+	if cmd.Flag("open").Value.String() != "" {
+		exec.Command("open", stdout.String()).Run()
+	}
+	if cmd.Flag("with-dev").Value.String() != "" {
+		exec.Command("open", strings.ReplaceAll(stdout.String(), "com/", "dev/")).Run()
+	}
 }
 
 func Execute() {
@@ -182,7 +197,7 @@ func execSemgrep(semgrepOption string, titleTemplate *string, bodyTemplate *stri
 }
 
 func init() {
-	rootCmd.Flags().StringP("repo", "r", "", "リポジトリ名")
+	rootCmd.Flags().StringP("repo", "r", "", "リポジトリ名 (カンマ区切りで複数指定可能)")
 	rootCmd.MarkFlagRequired("repo")
 	rootCmd.Flags().BoolP("force", "f", false, "cacheを削除して再取得します")
 	rootCmd.Flags().StringP("cmd", "c", "", "引数にあるコマンドを実行します")
